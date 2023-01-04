ec484fdb53c84d40da670185c0f4aca13a54f02f
floj <floj@users.noreply.github.com>
2023-01-04 09:34:36 +0100

    put secrets into keychanin (same as aws-vault does)

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/floj/totpgen
 
 go 1.15
 
-require github.com/pquerna/otp v1.3.0
+require (
+	github.com/99designs/keyring v1.2.2
+	github.com/pquerna/otp v1.3.0
+	github.com/urfave/cli/v2 v2.23.7
+)
--- main.go
+++ main.go
@@ -1,95 +1,159 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 
+	"github.com/99designs/keyring"
 	"github.com/pquerna/otp/totp"
+
+	"github.com/urfave/cli/v2"
 )
 
-type configEntry struct {
-	Name   string
-	Secret string
-}
-
-type config []configEntry
-
-func (c config) find(name string) *configEntry {
-	for _, e := range c {
-		if e.Name == name {
-			return &e
-		}
-	}
-	return nil
-}
-
-const usage = `
-Usage:
- Call with secret name
-   %s <secret-name>
-  or symlink to 'totpgen-secret-name'
-`
+const ringName = "totpgen"
 
 func main() {
-	token, err := run()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	appBin := filepath.Base(os.Args[0])
+	app := &cli.App{
+		Name:  appBin,
+		Usage: "prints a totp code for a configured secret",
+		Commands: []*cli.Command{
+			{
+				Name:      "set",
+				Usage:     "add or replace a configured secret",
+				UsageText: "totpgen set <secret-name> <secret-token>",
+				Action: func(ctx *cli.Context) error {
+					if ctx.Args().Len() != 2 {
+						cli.ShowSubcommandHelpAndExit(ctx, 1)
+					}
+					ring, err := openKeyring()
+					if err != nil {
+						return err
+					}
+					err = ring.Set(keyring.Item{
+						Key:  ctx.Args().Get(0),
+						Data: []byte(ctx.Args().Get(1)),
+					})
+					if err != nil {
+						return fmt.Errorf("could not insert new secret into keyring: %w", err)
+					}
+					return nil
+				},
+			},
+			{
+				Name:      "list",
+				Usage:     "show configured secrets",
+				UsageText: "totpgen list",
+				Action: func(ctx *cli.Context) error {
+					ring, err := openKeyring()
+					if err != nil {
+						return err
+					}
+					kk, err := ring.Keys()
+					if err != nil {
+						return fmt.Errorf("could not list keys from keyring: %w", err)
+					}
+					sort.Strings(kk)
+					for _, k := range kk {
+						fmt.Println(k)
+					}
+					return nil
+				},
+			},
+		},
+		UsageText: "totpgen <secret-name>\ntotpgen-<secret-name> (eg. symlinked)\ntotpgen command [command options]",
+		ArgsUsage: "<secret name>",
+		Action: func(ctx *cli.Context) error {
+			secName := ""
+			if idx := strings.Index(appBin, "-"); idx >= 0 {
+				secName = appBin[idx+1:]
+			} else {
+				secName = ctx.Args().First()
+			}
+
+			if secName == "" {
+				cli.ShowAppHelpAndExit(ctx, 1)
+			}
+
+			ring, err := openKeyring()
+			if err != nil {
+				return err
+			}
+			e, err := ring.Get(secName)
+			if err != nil {
+				return fmt.Errorf("could not get secret for '%s': %w", secName, err)
+			}
+
+			token, err := totp.GenerateCode(string(e.Data), time.Now())
+			if err != nil {
+				return fmt.Errorf("could not generate totp code for '%s': %w", secName, err)
+			}
+			fmt.Print(token)
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Print(token)
-
 }
 
-func run() (string, error) {
-	confName := getConfName(os.Args)
-	if confName == "" {
-		return "", fmt.Errorf(usage, os.Args[0])
-	}
-	conf, err := loadConfig()
+func openKeyring() (keyring.Keyring, error) {
+	ring, err := keyring.Open(keyring.Config{
+		ServiceName: ringName,
+	})
 	if err != nil {
-		return "", fmt.Errorf("Could not open config: %w", err)
+		return nil, fmt.Errorf("could not open keyring: %w", err)
 	}
-
-	entry := conf.find(confName)
-	if entry == nil {
-		return "", fmt.Errorf("No config entry for '%s' found", confName)
-	}
-
-	token, err := totp.GenerateCode(entry.Secret, time.Now())
-	return token, err
+	return ring, nil
 }
 
-func loadConfig() (*config, error) {
-	confDir, err := os.UserConfigDir()
-	if err != nil {
-		return nil, err
-	}
-	confFile := filepath.Join(confDir, "totpgen", "config.json")
-	f, err := os.Open(confFile)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
+// func main() {
+// 	addSecret := flag.Bool("set", false, "add/replace an OTP secret, use like -set <secret name> <secret token>")
+// 	flag.Parse()
 
-	conf := &config{}
-	err = json.NewDecoder(f).Decode(conf)
-	if err != nil {
-		return nil, err
-	}
-	return conf, nil
-}
+// 	token, err := run()
+// 	if err != nil {
+// 		fmt.Fprintln(os.Stderr, err)
+// 		os.Exit(1)
+// 	}
+// 	fmt.Print(token)
+// }
 
-func getConfName(args []string) string {
-	cmd := os.Args[0]
-	if idx := strings.Index(cmd, "-"); idx >= 0 {
-		return cmd[idx+1:]
-	}
-	if len(args) == 2 {
-		return args[1]
-	}
-	return ""
-}
+// func run() (string, error) {
+// 	ring, err := keyring.Open(keyring.Config{
+// 		ServiceName: "github.com/floj/totpgen",
+// 	})
+// 	if err != nil {
+// 		return "", fmt.Errorf("could not open keyring: %w", err)
+// 	}
+
+// 	confName := getConfName(os.Args)
+// 	if confName == "" {
+// 		return "", fmt.Errorf(usage, os.Args[0])
+// 	}
+
+// 	e, err := ring.Get(confName)
+// 	if err != nil {
+// 		return "", fmt.Errorf("could not get secret for '%s': %w", confName, err)
+// 	}
+
+// 	token, err := totp.GenerateCode(string(e.Data), time.Now())
+// 	return token, err
+// }
+
+// func getConfName(args []string) string {
+// 	cmd := os.Args[0]
+// 	if idx := strings.Index(cmd, "-"); idx >= 0 {
+// 		return cmd[idx+1:]
+// 	}
+// 	if len(args) == 2 {
+// 		return args[1]
+// 	}
+// 	return ""
+// }
