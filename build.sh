8aa160430578ab860e293dbb54da09fb80b8946e
floj <floj@users.noreply.github.com>
2024-03-03 10:53:23 +0100

    leave CGO enabled on MacOS (fixes #1)

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 scriptDir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
-export CGO_ENABLED=0
+case "$(uname -s)" in
+Darwin)
+	export CGO_ENABLED=1
+	;;
+*)
+	export CGO_ENABLED=0
+	;;
+esac
 go build -v -ldflags '-s -w' -trimpath -o totpgen "$scriptDir"
